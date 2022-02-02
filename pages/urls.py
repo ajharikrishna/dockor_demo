2a588ab9ba71bf2ad7d69428d02e6ad8bdffd0b1
ajharikrishna <aj.harikrishna@gmail.com>
2022-02-02 18:53:18 +0530

    url master added

--- pages/urls.py
+++ pages/urls.py
@@ -2,6 +2,8 @@ from unicodedata import name
 from django.urls import path
 from .views import home_page_view, home__page_view_sec
 
+master = 'master only'
+
 urlpatterns = [
     path('',home_page_view, name='home'),
     path('sec/', home__page_view_sec, name='sec')
